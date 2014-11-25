62a5d4e9c81a1a679d8ac1af54c950251a15c648
Ruslan Kaymakov <r.kaymakov@lenta-co.ru>
2014-11-25 14:51:44 +0300

    Fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,41 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    svgstore: {
+      options: {
+        prefix : 'icon-'
+      },
+      default : {
+        files: {
+          'assets/images/sprite.svg': ['tmp/*.svg'],
+        }
+      }
+    },
+    clean: {
+      svg: ["tmp"]
+    },
+    svgmin: {
+      options: {
+        plugins: [
+          {
+            removeViewBox: false
+          }, {
+            removeUselessStrokeAndFill: false
+          }
+        ]
+      },
+      multiple: {
+        files: [{
+          expand:true,
+          cwd: 'svg/',
+          src: ['**/*.svg'],
+          dest: 'tmp/'
+        }]
+      }
+    }
   });
   grunt.loadNpmTasks('grunt-svgstore');
-  grunt.registerTask('default', []);
-  grunt.registerTask('dev', []);
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-svgmin');
+  grunt.registerTask('default', ['svgmin', 'svgstore', 'clean']);
 };
